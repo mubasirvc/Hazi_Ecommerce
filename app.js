b2390034d2354bf47dcb6eb33cbf65bdf7f48db0
mubasir umbi <mubasirumbi@gmail.com>
2024-12-04 14:48:58 +0530

    removed bin file and main file changed to app.js

--- app.js
+++ app.js
@@ -1,121 +1,79 @@
-const createError = require('http-errors');
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const mongoose = require('mongoose')
-const hbs = require('hbs')
-const session = require('express-session')
-const Handlebars = require('handlebars')
-const exphbs = require('express-handlebars')
-const nocache = require('nocache')
-const multer = require('multer')
- require('dotenv').config()
-
-
-
-// mongoose.connect(process.env.MONGODB_URL)
-
+const mongoose = require('mongoose');
+const hbs = require('hbs');
+const session = require('express-session');
+const nocache = require('nocache');
 
 const userRouter = require('./routes/user');
 const adminRouter = require('./routes/admin');
 const connectDB = require('./config/db');
-// const { handlebars } = require('hbs');
 
-connectDB()
+// Connect to the database
+connectDB();
+
 const app = express();
 
-
-let hbss = exphbs.create({})
-
-
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-hbs.registerPartials(path.join(__dirname,'/views/partials'))
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
-
+// Handlebars helpers
 hbs.registerHelper('ifeq', function (a, b, options) {
-  if (a == b) { return options.fn(this); }
-  return options.inverse(this);
+  return a == b ? options.fn(this) : options.inverse(this);
 });
-
-
 hbs.registerHelper('ifnoteq', function (a, b, options) {
-  if (a != b) { return options.fn(this); }
-  return options.inverse(this);
+  return a != b ? options.fn(this) : options.inverse(this);
 });
-
-
 hbs.registerHelper("for", function (from, to, incr, block) {
   let accum = "";
   for (let i = from; i <= to; i += incr) {
     accum += block.fn(i);
   }
   return accum;
+});
+hbs.registerHelper('ifCond', function (v1, v2, options) {
+  return v1 === v2 ? options.fn(this) : options.inverse(this);
+});
 
-})
-
-
-hbs.registerHelper('ifCond', function(v1, v2, options) {
-  if (v1 === v2) {
-    return options.fn ? options.fn(this) : options.fn;
-  } else {
-    return options.inverse ? options.inverse(this) : options.inverse
-  }
-})
-
-
-// hbs.registerHelper("json", function (context) {
-//   return JSON.stringify(context)
-// })
-
-
+// Middleware setup
 app.use(session({
   secret: process.env.KEY,
   saveUninitialized: true,
-   cookie: { maxAge: 600000000 },
-  resave: false 
+  cookie: { maxAge: 600000000 },
+  resave: false
 }));
-
-
 app.use(nocache());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-
 app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
-// app.use(multer({dest: 'uploads', storage: fileStorage, fileFilter: fileFilter}).single('image'))
 
+// Routes
 app.use('/', userRouter);
 app.use('/admin', adminRouter);
 
-// catch 404 and forward to error handler
-
-app.use(function(req, res, next) {
+// 404 handler
+app.use((req, res, next) => {
   res.status(404).render('404');
 });
 
-
-// error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-
-
-app.use(function(err, req, res, next) {
+// Error handler
+app.use((err, req, res, next) => {
   res.status(500);
   res.render('error', { error: err });
 });
- 
+
+// Server setup
+const PORT = process.env.PORT || 3005;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
